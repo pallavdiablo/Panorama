bdc314108c62b92052c1605e21dc50a6e5ccc72f
pallavdiablo <pallav.iitg@gmail.com>
2015-09-14 13:28:58 +0530

    Delete model.Closure.php

--- models/model.Closure.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Closure {
-		public $id, $name;
-		public $program_closure_date, $program_closure_comment;
-
-		public function __construct( $id = -1);		// set $this->id = $id; populate all member vars from DB
-	}
-?>
